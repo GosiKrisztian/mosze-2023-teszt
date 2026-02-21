4b88883270e4520ca4e1b95d70f75c11454ef790
GosiKrisztian <gosikrisz@gmail.com>
2026-02-21 11:04:56 +0100

    kód fix

--- main.cpp
+++ main.cpp
@@ -4,32 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // NELEMENTS helyett N_ELEMENTS kéne
-    int *b = new int[NELEMENTS];
-    // pontosvessző hiányzik a sor végéről
-    std::cout << '1-100 ertekek duplazasa'
-    // nincs megadva feltétel a ciklusnak
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // pontosvessző hiányzik a sor végéről
         b[i] = i * 2;
     }
-    // a feltétel rossz, ezért nem futna le a ciklus, és vessző kéne pontosvessző helyett
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // pontosvessző hiányzik a sor végéről
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    // nincs kezdőérték megadva az atlag váltózónak
-    int atlag;
-    // vessző kéne pontosvessző helyett
-    for (int i = 0; i < N_ELEMENTS, i++)
+    
+    int atlag = 0;
+    
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // pontosvessző hiányzik a sor végéről
-        atlag += b[i]
+        atlag += b[i];
     }
-    // tizedes érték nem lesz mert integer az atlag változó
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
